d357e3ed36ebc4d052224f07cd6a654830a5cc52
Владимир <vova.denezhkin.98@mail.ru>
2020-09-05 16:24:09 +0300

    дз 003 третья доработка

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -9,29 +9,26 @@ import simple_draw
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-def smiley(point, color):
+def smiley(x, y, color):
     radius = 50
-    simple_draw.circle(center_position=point[0], radius=radius, color=color, width=1)
+    simple_draw.circle(center_position=simple_draw.get_point(x, y), radius=radius, color=color, width=1)
     radius -= 40
-    simple_draw.circle(center_position=point[1], radius=radius, color=simple_draw.COLOR_DARK_PURPLE, width=3)
-    simple_draw.circle(center_position=point[2], radius=radius, color=simple_draw.COLOR_DARK_PURPLE, width=3)
-    simple_draw.line(start_point=point[3], end_point=point[4], color=color, width=3)
-    simple_draw.line(start_point=point[4], end_point=point[5], color=color, width=3)
-    simple_draw.line(start_point=point[5], end_point=point[6], color=color, width=3)
+    simple_draw.circle(center_position=simple_draw.get_point(x - 25, y + 20), radius=radius,
+                       color=simple_draw.COLOR_DARK_PURPLE, width=3)
+    simple_draw.circle(center_position=simple_draw.get_point(x + 25, y + 20), radius=radius,
+                       color=simple_draw.COLOR_DARK_PURPLE, width=3)
+    simple_draw.line(start_point=simple_draw.get_point(x - 20, y - 15), end_point=simple_draw.get_point(x - 10, y - 25),
+                     color=color, width=3)
+    simple_draw.line(start_point=simple_draw.get_point(x - 10, y - 25), end_point=simple_draw.get_point(x + 10, y - 25),
+                     color=color, width=3)
+    simple_draw.line(start_point=simple_draw.get_point(x + 10, y - 25), end_point=simple_draw.get_point(x + 20, y - 15),
+                     color=color, width=3)
 
 
 for _ in range(10):
     color = simple_draw.COLOR_YELLOW
     x = simple_draw.random_number(0, 550)
     y = simple_draw.random_number(0, 550)
-    # TODO этот список должен быть задан внутри функции, передавать его аргументом - решение ошибочное потому, что
-    # TODO представьте, Вам метод отрисовки смайла нужно вызывать много раз и каждый раз копипастить этот список
-    # TODO будет весьма накладно. Поэтому лучше его сделать частью функции, а в качестве параметров вместо point=point
-    # TODO передавать только x и y или объект Point, сформированный на основе этих координат
-    point = [simple_draw.get_point(x, y), simple_draw.get_point(x - 25, y + 20),
-             simple_draw.get_point(x + 25, y + 20), simple_draw.get_point(x - 20, y - 15),
-             simple_draw.get_point(x - 10, y - 25), simple_draw.get_point(x + 10, y - 25),
-             simple_draw.get_point(x + 20, y - 15)]
-    smiley(point=point, color=color)
+    smiley(x=x, y=y, color=color)
 
 simple_draw.pause()
