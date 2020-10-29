43021ff87b6d42f4abb7d9defe6e56dc231a9133
aguseva <flutesa@ya.ru>
2020-10-29 20:24:04 +0100

    задания нужно доработать

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -10,20 +10,23 @@
 
 from district.central_street.house1 import room1
 from district.central_street.house1 import room2
+
 central_house1 = room1.folks + room2.folks
 from district.central_street.house2 import room1
 from district.central_street.house2 import room2
+
 central_house2 = room1.folks + room2.folks
 from district.soviet_street.house1 import room1
 from district.soviet_street.house1 import room2
+
 soviet_house1 = room1.folks + room2.folks
 from district.soviet_street.house2 import room1
 from district.soviet_street.house2 import room2
+
 soviet_house2 = room1.folks + room2.folks
 
 people = central_house1 + central_house2 + soviet_house1 + soviet_house2
 
-print('На районе живут', ', ' .join(people))
+print('На районе живут', ', '.join(people))
 
-
-# TODO здесь ваш код
+# TODO подключение модулей должно быть всегда выше всех остальных строк кода, их использующих
