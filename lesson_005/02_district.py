cae34d0aa220c9754641b3caf9d80dc65d5bbf83
Владимир <vova.denezhkin.98@mail.ru>
2020-10-30 15:26:19 +0300

    005 дз доработка кроме 04

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -6,27 +6,22 @@
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
 
-# people = []
+import district.central_street.house1.room1
+import district.central_street.house1.room2
+import district.central_street.house2.room1
+import district.central_street.house2.room2
+import district.soviet_street.house1.room1
+import district.soviet_street.house1.room2
+import district.soviet_street.house2.room1
+import district.soviet_street.house2.room2
 
-from district.central_street.house1 import room1
-from district.central_street.house1 import room2
-
-central_house1 = room1.folks + room2.folks
-from district.central_street.house2 import room1
-from district.central_street.house2 import room2
-
-central_house2 = room1.folks + room2.folks
-from district.soviet_street.house1 import room1
-from district.soviet_street.house1 import room2
-
-soviet_house1 = room1.folks + room2.folks
-from district.soviet_street.house2 import room1
-from district.soviet_street.house2 import room2
-
-soviet_house2 = room1.folks + room2.folks
+central_house1 = district.central_street.house1.room1.folks + district.central_street.house1.room2.folks
+central_house2 = district.central_street.house2.room1.folks + district.central_street.house2.room2.folks
+soviet_house1 = district.soviet_street.house1.room1.folks + district.soviet_street.house1.room2.folks
+soviet_house2 = district.soviet_street.house2.room1.folks + district.soviet_street.house2.room2.folks
 
 people = central_house1 + central_house2 + soviet_house1 + soviet_house2
 
 print('На районе живут', ', '.join(people))
 
-# TODO подключение модулей должно быть всегда выше всех остальных строк кода, их использующих
+
