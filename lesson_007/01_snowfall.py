27aa62dc9539fa763fff0765c2bd041af8e35984
Владимир <vova.denezhkin.98@mail.ru>
2020-12-27 17:02:40 +0300

    007 01 часть дз

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -1,15 +1,47 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+from random import randint
 
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
 #  - отработку изменений координат
 #  - отрисовку
 
+snow_list = []
+for _ in range(20):
+    snow_list.append([randint(80, 800), randint(500, 700), randint(20, 60)])
+
 
 class Snowflake:
-    pass
+    def clear_previous_picture(self):
+        sd.start_drawing()
+        self.color = sd.background_color
+        for coordinate in snow_list:
+            self.length = coordinate[2]
+            self.point = sd.get_point(coordinate[0], coordinate[1])
+            sd.snowflake(center=self.point, length=self.length, color=self.color)
+        sd.finish_drawing()
+
+    def move(self):
+        for self.coordinate in snow_list:
+            self.coordinate[1] -= 2
+
+    def draw(self):
+        sd.start_drawing()
+        self.color = sd.COLOR_WHITE
+        for coordinate in snow_list:
+            self.length = coordinate[2]
+            self.point = sd.get_point(coordinate[0], coordinate[1])
+            sd.snowflake(center=self.point, length=self.length, color=self.color)
+        sd.finish_drawing()
+
+    def can_fall(self):
+        for self.coordinate in snow_list:
+            if self.coordinate[1] > 10:
+                return True
+            else:
+                return False
 
     # TODO здесь ваш код
 
@@ -22,7 +54,7 @@ while True:
     flake.draw()
     if not flake.can_fall():
         break
-    sd.sleep(0.1)
+    sd.sleep(0.01)
     if sd.user_want_exit():
         break
 
