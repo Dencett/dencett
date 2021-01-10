38650ad1d929ab89f71d89fe31ecda5e49ac8c9f
Владимир <vova.denezhkin.98@mail.ru>
2021-01-10 21:09:30 +0300

    007, 01 и 03 дз

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -8,55 +8,56 @@ from random import randint
 #  - отработку изменений координат
 #  - отрисовку
 
+N = 20
+
 snow_list = []
-for _ in range(20):
+for _ in range(N):
     snow_list.append([randint(80, 800), randint(500, 700), randint(20, 60)])
 
 
 class Snowflake:
-    def clear_previous_picture(self):
+    def clear_previous_picture(self, coordinate):
         sd.start_drawing()
         self.color = sd.background_color
-        for coordinate in snow_list:
-            self.length = coordinate[2]
-            self.point = sd.get_point(coordinate[0], coordinate[1])
-            sd.snowflake(center=self.point, length=self.length, color=self.color)
+        self.length = coordinate[2]
+        self.point = sd.get_point(coordinate[0], coordinate[1])
+        sd.snowflake(center=self.point, length=self.length, color=self.color)
         sd.finish_drawing()
 
-    def move(self):
-        for self.coordinate in snow_list:
-            self.coordinate[1] -= 2
+    def move(self, coordinate):
+        coordinate[1] -= 2
 
-    def draw(self):
+    def draw(self, coordinate):
         sd.start_drawing()
         self.color = sd.COLOR_WHITE
-        for coordinate in snow_list:
-            self.length = coordinate[2]
-            self.point = sd.get_point(coordinate[0], coordinate[1])
-            sd.snowflake(center=self.point, length=self.length, color=self.color)
+        self.length = coordinate[2]
+        self.point = sd.get_point(coordinate[0], coordinate[1])
+        sd.snowflake(center=self.point, length=self.length, color=self.color)
         sd.finish_drawing()
 
-    def can_fall(self):
-        for self.coordinate in snow_list:
-            if self.coordinate[1] > 10:
-                return True
-            else:
-                return False
+    def can_fall(self, coordinate):
+        if coordinate[1] > -60:
+            return True
+        else:
+            return False
 
     # TODO здесь ваш код
 
 
 flake = Snowflake()
 
-while True:
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
-    if not flake.can_fall():
-        break
-    sd.sleep(0.01)
-    if sd.user_want_exit():
-        break
+
+# coordinate = ([randint(80, 800), randint(500, 700), randint(20, 60)])
+#
+# while True:
+#     flake.clear_previous_picture(coordinate)
+#     flake.move(coordinate)
+#     flake.draw(coordinate)
+#     if not flake.can_fall(coordinate):
+#         break
+#     sd.sleep(0.01)
+#     if sd.user_want_exit():
+#         break
 
 # шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
 # flakes = get_flakes(count=N)  # создать список снежинок
@@ -71,5 +72,30 @@ while True:
 #     sd.sleep(0.1)
 #     if sd.user_want_exit():
 #         break
+
+N = 20
+snow_list = []
+for _ in range(N):
+    snow_list.append([randint(80, 800), randint(500, 700), randint(20, 60)])
+
+
+def get_fallen_flakes():
+    for coordinate in snow_list:
+        if coordinate[1] == -60:
+            snow_list.append([randint(80, 800), randint(500, 700), randint(20, 60)])
+
+
+while True:
+    for coordinate in snow_list:
+        flake.clear_previous_picture(coordinate)
+        flake.move(coordinate)
+        flake.draw(coordinate)
+    get_fallen_flakes()
+    # fallen_flakes = get_fallen_flakes()
+    # if fallen_flakes:
+    #     append_flakes(count=fallen_flakes)
+    sd.sleep(0.01)
+    if sd.user_want_exit():
+        break
 
 sd.pause()
--- lesson_007/03_man_ans_cat.py
+++ lesson_007/03_man_ans_cat.py
@@ -25,10 +25,187 @@ from random import randint
 
 # Человеку и коту надо вместе прожить 365 дней.
 
-# TODO здесь ваш код
+from termcolor import cprint
+
+
+class Resident:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+
+    def __str__(self):
+        return '{}, сытость {}'.format(
+            self.name, self.fullness,
+        )
+
+    def eat(self):
+        if self.house.food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 20
+            self.house.food -= 10
+        else:
+            cprint('{} нет еды'.format(self.name), color='red')
+            self.fullness -= 10
+
+    def work(self):
+        cprint('{} сходил на работу'.format(self.name), color='blue')
+        self.house.money += 150
+        self.fullness -= 10
+
+    def relax(self):
+        cprint('{} отдыхал'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def shopping(self):
+        if self.house.money >= 50:
+            if self.house.food <= 10:
+                cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
+                self.house.money -= 50
+                self.house.food += 50
+            if self.house.cat_food <= 20:
+                cprint('{} сходил в магазин за кошачьей едой'.format(self.name), color='magenta')
+                self.house.money -= 50
+                self.house.cat_food += 50
+        else:
+            cprint('{} деньги кончились!'.format(self.name), color='red')
+
+    def cleaning(self):
+        cprint('{} убрался в доме'.format(self.name), color='magenta')
+        self.house.mud -= 100
+        if self.house.mud < 0:
+            self.house.mud = 0
+        self.fullness -= 20
+
+    def resident_house(self, house):
+        self.house = house
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint('{} умер...'.format(self.name), color='red')
+            return
+        dice = randint(1, 4)
+        if self.fullness <= 30:
+            self.eat()
+        elif self.house.food < 20:
+            self.shopping()
+        elif self.house.cat_food < 30:
+            self.shopping()
+        elif self.house.money < 50:
+            self.work()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        elif dice == 3:
+            self.cleaning()
+        else:
+            self.relax()
+
+
+class Cat:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+
+    def __str__(self):
+        return '{}, сытость {}'.format(
+            self.name, self.fullness,
+        )
+
+    def sleep(self):
+        cprint('{} поспал.'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def pick_cat(self, house):
+        self.house = house
+        cprint('Хозяин подобрал кота, дал имя "{}"'.format(self.name), color='cyan')
+
+    def eat(self):
+        if self.house.cat_food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 10
+            self.house.cat_food -= 10
+        else:
+            cprint('{} мяукает, нет еды'.format(self.name), color='red')
+            self.fullness -= 10
+
+    def tear_wallpaper(self):
+        cprint('{} дерет обои'.format(self.name), color='yellow')
+        self.house.mud += 5
+        self.fullness -= 10
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint('{} умер...'.format(self.name), color='red')
+            return
+        dice = randint(1, 4)
+        if self.fullness <= 20:
+            self.eat()
+        elif dice == 1:
+            self.eat()
+        elif dice == 2:
+            self.tear_wallpaper()
+        else:
+            self.sleep()
+
+
+class House:
+
+    def __init__(self):
+        self.food = 50
+        self.cat_food = 0
+        self.money = 100
+        self.mud = 0
+
+    def __str__(self):
+        return 'В доме еды осталось {}, кошачьей еды {}, денег осталось {}, грязи {}'.format(
+            self.food, self.cat_food, self.money, self.mud
+        )
+
+
+householder = Resident(name='Хозяин')
+house = House()
+householder.resident_house(house=house)
+
+# pet = Cat(name='Барсик')
+# pet.pick_cat(house=house)
+#
+# for day in range(1, 366):
+#     print('================ день {} =================='.format(day))
+#     householder.act()
+#     pet.act()
+#     print('--- в конце дня ---')
+#     print(householder)
+#     print(pet)
+#     print(house)
 
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.
 # Им всем вместе так же надо прожить 365 дней.
+
+cats = [
+    Cat(name='Барсик'),
+    Cat(name='Мурзик'),
+    Cat(name='Снежок'),
+]
+
+for cat in cats:
+    cat.pick_cat(house=house)
+
+for day in range(1, 366):
+    print('================ день {} =================='.format(day))
+    householder.act()
+    for cat in cats:
+        cat.act()
+    print('--- в конце дня ---')
+    print(householder)
+    for cat in cats:
+        print(cat)
+    print(house)
+
 
 # (Можно определить критическое количество котов, которое может прокормить человек...)
