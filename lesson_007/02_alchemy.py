1bbac1f7634e46ed84ddd51a6be3a07811698812
Владимир <vova.denezhkin.98@mail.ru>
2021-02-04 20:43:28 +0300

    007, 02

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,7 +19,171 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+
+class Water:
+
+    def __init__(self):
+        self.element = 'Вода'
+
+    def __str__(self):
+        return self.element
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        if isinstance(other, Fire):
+            return Steam()
+        if isinstance(other, Earth):
+            return Mud()
+        if isinstance(other, Water):
+            return Lake()
+
+
+class Air:
+
+    def __init__(self):
+        self.element = 'Воздух'
+
+    def __str__(self):
+        return self.element
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Storm()
+        if isinstance(other, Fire):
+            return Lightning()
+        if isinstance(other, Earth):
+            return Dust()
+        if isinstance(other, Air):
+            return Wind()
+
+
+class Fire:
+
+    def __init__(self):
+        self.element = 'Огонь'
+
+    def __str__(self):
+        return self.element
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Steam()
+        if isinstance(other, Air):
+            return Lightning()
+        if isinstance(other, Earth):
+            return Lava()
+
+
+class Earth:
+
+    def __init__(self):
+        self.element = 'Земля'
+
+    def __str__(self):
+        return self.element
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Mud()
+        if isinstance(other, Air):
+            return Dust()
+        if isinstance(other, Fire):
+            return Lava()
+        if isinstance(other, Earth):
+            return Pressure()
+
+
+class Storm:
+
+    def __init__(self):
+        self.element = 'Шторм'
+
+    def __str__(self):
+        return self.element
+
+
+class Steam:
+
+    def __init__(self):
+        self.element = 'Пар'
+
+    def __str__(self):
+        return self.element
+
+
+class Mud:
+
+    def __init__(self):
+        self.element = 'Грязь'
+
+    def __str__(self):
+        return self.element
+
+
+class Lightning:
+
+    def __init__(self):
+        self.element = 'Молния'
+
+    def __str__(self):
+        return self.element
+
+
+class Dust:
+
+    def __init__(self):
+        self.element = 'Пыль'
+
+    def __str__(self):
+        return self.element
+
+
+class Lava:
+
+    def __init__(self):
+        self.element = 'Лава'
+
+    def __str__(self):
+        return self.element
+
+
+class Wind:
+
+    def __init__(self):
+        self.element = 'Ветер'
+
+    def __str__(self):
+        return self.element
+
+
+class Pressure:
+
+    def __init__(self):
+        self.element = 'Давление'
+
+    def __str__(self):
+        return self.element
+
+
+class Lake:
+
+    def __init__(self):
+        self.element = 'Озеро'
+
+    def __str__(self):
+        return self.element
+
+
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Water(), '+', Fire(), '=', Water() + Fire())
+print(Water(), '+', Earth(), '=', Water() + Earth())
+print(Air(), '+', Fire(), '=', Air() + Fire())
+print(Air(), '+', Earth(), '=', Air() + Earth())
+print(Fire(), '+', Earth(), '=', Fire() + Earth())
+print(Water(), '+', Water(), '=', Water() + Water())
+print(Air(), '+', Air(), '=', Air() + Air())
+print(Earth(), '+', Earth(), '=', Earth() + Earth())
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
