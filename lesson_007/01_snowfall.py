b69fc6044c69812b8ef191ae56bb4f98fd2cc503
Владимир <vova.denezhkin.98@mail.ru>
2021-02-11 20:48:47 +0300

    007, 01 доработка

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -13,47 +13,46 @@ import simple_draw as sd
 
 class Snowflake:
 
-    def __init__(self, coordinate):
-        self.coordinate = coordinate
+    def __init__(self, x, y, length, change_coordinate):
+        self.x = x
+        self.y = y
+        self.length = length
+        self.change_coordinate = change_coordinate
 
     def clear_previous_picture(self):
         sd.start_drawing()
         self.color = sd.background_color
-        self.length = coordinate[2]
-        self.point = sd.get_point(coordinate[0], coordinate[1])
+        self.length = self.length
+        self.point = sd.get_point(self.x, self.y)
         sd.snowflake(center=self.point, length=self.length, color=self.color)
         sd.finish_drawing()
 
-    def move(self, change_coordinate):
-        coordinate[1] -= change_coordinate
+    def move(self):
+        self.y -= self.change_coordinate
 
     def draw(self):
         self.color = sd.COLOR_WHITE
-        self.length = coordinate[2]
-        self.point = sd.get_point(coordinate[0], coordinate[1])
+        self.point = sd.get_point(self.x, self.y)
         sd.snowflake(center=self.point, length=self.length, color=self.color)
 
     def can_fall(self):
-        return coordinate[1] > 0  # так тоже можно
+        return self.y > 0  # так тоже можно
 
 
-# TODO эти параметры снежинки должны быть в конструкторе (метод __init__)
-coordinate = ([randint(80, 800), randint(500, 700), randint(20, 60)])
-flake = Snowflake(coordinate)  # TODO передавать их как аргумент - подход неверный
-# TODO при создании массива снежинок это создаст ненужную путаницу
-#  а так, если все координаты и длины для каждой снежинки - "внутри" её самой спрятаны - будет гораздо проще
+# flake = Snowflake(randint(80, 800), randint(500, 700), randint(20, 60), randint(1, 4))
 
-while True:
-    sd.start_drawing()
-    flake.clear_previous_picture()
-    flake.move(randint(1, 4))
-    flake.draw()
-    if not flake.can_fall():
-        break
-    sd.finish_drawing()
-    sd.sleep(0.01)
-    if sd.user_want_exit():
-        break
+
+# while True:
+#     sd.start_drawing()
+#     flake.clear_previous_picture()
+#     flake.move()
+#     flake.draw()
+#     if not flake.can_fall():
+#         break
+#     sd.finish_drawing()
+#     sd.sleep(0.01)
+#     if sd.user_want_exit():
+#         break
 
 # зачёт первой части
 
@@ -72,38 +71,45 @@ while True:
 #     if sd.user_want_exit():
 #         break
 
-# N = 20
-#
-#
-# def get_flakes(coordinate):
-#     snow_list = []
-#     for _ in range(coordinate):
-#         snow_list.append([randint(80, 800), randint(500, 700), randint(20, 60)])
-#     return snow_list
+N = 20
 
 
-# flakes = get_flakes(N)
-#
-#
-# flake = Snowflake(snow_list)
-# def get_fallen_flakes():
-#     for coordinate in snow_list:
-#         if not flake.can_fall():  # TODO здесь нужно использовать метод can_fall
-#             snow_list.append([randint(80, 800), randint(500, 700), randint(20, 60)])
-#             # flake = Snowflake(coordinate)
-#
-#
-# while True:
-#     for flake in flakes:
-#         flake.clear_previous_picture()
-#         flake.move()
-#         flake.draw()
-#     get_fallen_flakes()
-#     fallen_flakes = get_fallen_flakes()
-#     if fallen_flakes:
-#         append_flakes(count=fallen_flakes)
-#     sd.sleep(0.01)
-#     if sd.user_want_exit():
-#         break
+def get_flakes(quantity):
+    snow_list = []
+    for _ in range(quantity):
+        snow_list.append(Snowflake(randint(80, 550), randint(600, 700), randint(20, 60), randint(2, 3)))
+    return snow_list
+
+
+flakes = get_flakes(N)
+print(flakes)
+
+
+def get_fallen_flakes():
+    quantity = 0
+    for flake in reversed(flakes):
+        if not flake.can_fall():
+            flakes.remove(flake)
+            quantity += 1
+            return quantity
+
+
+def append_flakes(quantity):
+    for _ in range(quantity):
+        flakes.append(Snowflake(randint(80, 550), randint(600, 700), randint(20, 60), randint(2, 3)))
+
+
+
+while True:
+    for flake in flakes:
+        flake.clear_previous_picture()
+        flake.move()
+        flake.draw()
+    fallen_flakes = get_fallen_flakes()
+    if fallen_flakes:
+        append_flakes(fallen_flakes)
+    sd.sleep(0.01)
+    if sd.user_want_exit():
+        break
 
 sd.pause()
