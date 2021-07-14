2d53a429a13bca0aef7fdb466372437d39cc08cf
Владимир <vova.denezhkin.98@mail.ru>
2021-07-14 20:31:29 +0300

    012 задание 3

--- lesson_012/03_volatility_with_processes.py
+++ lesson_012/03_volatility_with_processes.py
@@ -17,6 +17,80 @@
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
 #
-# TODO Внимание! это задание можно выполнять только после зачета lesson_012/02_volatility_with_threads.py !!!
 
-# TODO тут ваш код в многопроцессном стиле
+import os
+from operator import itemgetter
+from multiprocessing import Process, Pipe
+
+volatility_dict = {}
+zero_volatility = []
+
+
+class Trader(Process):
+    def __init__(self, path, conn):
+        super(Trader, self).__init__()
+        self.path = path
+        self.conn = conn
+
+    def run(self):
+        with open(self.path) as file:
+            next(file)
+            line = next(file)
+            secid, tradetime, price, quantity = line.split(',')
+            price_min = float(price)
+            price_max = float(price)
+            for line in file:
+                secid, tradetime, price, quantity = line.split(',')
+                price = float(price)
+                if price_min > price:
+                    price_min = price
+                if price_max < price:
+                    price_max = price
+            half_sum = (price_max + price_min) / 2
+            volatility = round(((price_max - price_min) / half_sum) * 100, 2)
+            self.conn.send([secid, volatility])
+
+
+def get_path_list(path):
+    file_list = []
+    for name, dirs, files in os.walk(path):
+        for file in files:
+            path = os.path.join(name, file)
+            file_list.append(path)
+    return file_list
+
+
+def measure_volatility():
+    order_of_volatility = sorted(volatility_dict.items(), key=itemgetter(1), reverse=True)
+    print('Максимальная волатильность:')
+    for ticker, volatility in order_of_volatility[:3]:
+        print(f'{ticker} {volatility} %')
+    print('Минимальная волатильность:')
+    for ticker, volatility in order_of_volatility[-1:-4:-1]:
+        print(f'{ticker} {volatility} %')
+    print('Нулевая волатильность:')
+    print(', '.join(zero_volatility))
+
+
+def tickers_files(path):
+    tickers, pipes = [], []
+    parent_conn, child_conn = Pipe()
+    for file in get_path_list(path):
+        ticker = Trader(file, child_conn)
+        tickers.append(ticker)
+        pipes.append(parent_conn)
+    for ticker in tickers:
+        ticker.start()
+    for conn in pipes:
+        secid, volatility = conn.recv()
+        if volatility > 0:
+            volatility_dict[secid] = volatility
+        else:
+            zero_volatility.append(secid)
+    for ticker in tickers:
+        ticker.join()
+    measure_volatility()
+
+
+if __name__ == '__main__':
+    tickers_files('trades')
