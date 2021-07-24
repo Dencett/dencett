a5f9dcc5801c45df89f065e30f496c789870de1d
Владимир <vova.denezhkin.98@mail.ru>
2021-07-24 20:56:52 +0300

    013 доработка 2

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -10,28 +10,29 @@
 
 import os
 from PIL import Image, ImageDraw, ImageFont, ImageColor
+import argparse
 
 TICKET = os.path.join('images', 'ticket_template.png')
 FONT_PATH = 'ofont.ru_Roboto.ttf'
 FILLED_TICKET = 'ticker.png'
 
 
-def make_ticket(fio, from_, to, date):
-    image = Image.open(TICKET)
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype(FONT_PATH, size=14)
-    data_dict = {fio: (50, 125), from_: (50, 194), to: (50, 260), date: (285, 260)}  # отлично!
-    for data, location in data_dict.items():
-        draw.text(location, data.upper(), font=font, fill=ImageColor.colormap['black'])
-    image.save(FILLED_TICKET)
-
-
-if __name__ == '__main__':
-    fio = input('Ф.И.О.: ')
-    from_ = input('Откуда: ')
-    to = input('Куда: ')
-    date = input('Дата: ')
-    make_ticket(fio, from_, to, date)
+# def make_ticket(fio, from_, to, date):
+#     image = Image.open(TICKET)
+#     draw = ImageDraw.Draw(image)
+#     font = ImageFont.truetype(FONT_PATH, size=14)
+#     data_dict = {fio: (50, 125), from_: (50, 194), to: (50, 260), date: (285, 260)}  # отлично!
+#     for data, location in data_dict.items():
+#         draw.text(location, data.upper(), font=font, fill=ImageColor.colormap['black'])
+#     image.save(FILLED_TICKET)
+#
+#
+# if __name__ == '__main__':
+#     fio = input('Ф.И.О.: ')
+#     from_ = input('Откуда: ')
+#     to = input('Куда: ')
+#     date = input('Дата: ')
+#     make_ticket(fio, from_, to, date)
 
 # зачет!
 
@@ -44,3 +45,27 @@ if __name__ == '__main__':
 #   --date - обязательный, когда летим.
 #   --save_to - необязательный, путь для сохранения заполненнего билета.
 # и заполнять билет.
+
+# def make_ticket(fio, from_, to, date, save_to):
+#     image = Image.open(TICKET)
+#     draw = ImageDraw.Draw(image)
+#     font = ImageFont.truetype(FONT_PATH, size=14)
+#     data_dict = {fio: (50, 125), from_: (50, 194), to: (50, 260), date: (285, 260)}  # отлично!
+#     for data, location in data_dict.items():
+#         draw.text(location, data.upper(), font=font, fill=ImageColor.colormap['black'])
+#     image.save(save_to)
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fio')
+    parser.add_argument('from_')
+    parser.add_argument('to')
+    parser.add_argument('date')
+    parser.add_argument('-s', '--save_to', default=FILLED_TICKET)
+    return parser
+
+
+if __name__ == '__main__':
+    parser = createParser()
+    ticket_parser = parser.parse_args()
+    print(ticket_parser.fio, ticket_parser.from_, ticket_parser.to, ticket_parser.date, ticket_parser.save_to)
